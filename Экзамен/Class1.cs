0d6d6798f12337432dd44610608627101054d6b8
Анастасия Петуненкова <apetunenkovaa@mail.ru>
2022-09-06 15:45:53 +0300

    Добавление диалога с пользователем и файла с UML

---
+++ Экзамен/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Экзамен
+{
+    internal class Class1
+    {
+    }
+}
--- Экзамен/Program.cs
+++ Экзамен/Program.cs
@@ -3,13 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Экзамен
 {
     internal class Program
     {
+        private static object otv;
+
         static void Main(string[] args)
         {
+            MessageBox.Show("Программа запущена ", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            while (true)
+            {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                try
+                {
+                    Console.Write("\nЖелаете повторить программу?\nДа(Y)/Нет(N)\nОтвет: ");
+                    otv = Convert.ToChar(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Введены некорректные данные!");
+                }
+            }
+            if ((otv.Equals('N') || otv.Equals('N') || otv.Equals('Т') || otv.Equals('т')))
+            {
+                return;
+            }
         }
-    }
+    }   
 }
